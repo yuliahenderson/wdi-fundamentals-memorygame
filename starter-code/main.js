0db80273987483f932d4e24eddc1aeced9548122
Julia Henderson <yulia.p.henderson@gmail.com>
2016-08-04 19:26:06 -0400

    game logic added to the main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,47 @@
+
 var  cardOne = "qeen";
 var cardTwo = "qeen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert ('You found a match!');
+}
+else if (cardTwo !== cardFour) {
+	alert ('Sorry, try again.');
+}
+
+if (cardOne === cardThree) {
+	alert ('You found a match!');
+}
+else if (cardOne !== cardThree) {
+	alert ('Sorry, try again.');
+}
+
+if (cardOne === cardTwo) {
+	alert ('You found a match!');
+}
+else if (cardOne !== cardTwo) {
+	alert ('Sorry, try again.');
+}
+
+if (cardThree === cardFour) {
+	alert ('You found a match!');
+}
+else if (cardThree !== cardFour) {
+	alert ('Sorry, try again.');
+}
+
+if (cardOne === cardThree) {
+	alert ('You found a match!');}
+
+else if (cardOne !== cardThree) {
+	alert ('Sorry, try again.');
+}
+
+if (cardTwo === cardThree) {
+	alert ('You found a match');
+}
+else if (cardTwo !== cardThree) {
+	alert ('Sorry, try again');
+}
