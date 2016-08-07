3e5c480f8c93efafde10140dc9d053e45239bc90
Julia Henderson <yulia.p.henderson@gmail.com>
2016-08-07 09:02:31 -0400

    updated code

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,41 +7,41 @@ var cardFour = "king";
 if (cardTwo === cardFour) {
 	alert ('You found a match!');
 }
-else if (cardTwo !== cardFour) {
+else {
 	alert ('Sorry, try again.');
 }
 
 if (cardOne === cardThree) {
 	alert ('You found a match!');
 }
-else if (cardOne !== cardThree) {
+else {
 	alert ('Sorry, try again.');
 }
 
 if (cardOne === cardTwo) {
 	alert ('You found a match!');
 }
-else if (cardOne !== cardTwo) {
+else {
 	alert ('Sorry, try again.');
 }
 
 if (cardThree === cardFour) {
 	alert ('You found a match!');
 }
-else if (cardThree !== cardFour) {
+else {
 	alert ('Sorry, try again.');
 }
 
 if (cardOne === cardThree) {
 	alert ('You found a match!');}
 
-else if (cardOne !== cardThree) {
+else  {
 	alert ('Sorry, try again.');
 }
 
 if (cardTwo === cardThree) {
 	alert ('You found a match');
 }
-else if (cardTwo !== cardThree) {
+else {
 	alert ('Sorry, try again');
 }
