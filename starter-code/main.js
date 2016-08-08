e39841a1da48709e1e4736d020d4b65ffcecaf37
Julia Henderson <yulia.p.henderson@gmail.com>
2016-08-08 15:52:05 -0400

    updated code Unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,47 +1,66 @@
 
-var  cardOne = "qeen";
-var cardTwo = "qeen";
-var cardThree = "king";
-var cardFour = "king";
+// var  cardOne = "qeen";
+// var cardTwo = "qeen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	alert ('You found a match!');
-}
-else {
-	alert ('Sorry, try again.');
-}
+function createBoard() {
 
-if (cardOne === cardThree) {
-	alert ('You found a match!');
-}
-else {
-	alert ('Sorry, try again.');
-}
+var board = document.getElementById('game-board');
+board.classList.toggle('show');
 
-if (cardOne === cardTwo) {
-	alert ('You found a match!');
-}
-else {
-	alert ('Sorry, try again.');
+var cards = ['queen', 'queen', 'king', 'king'];
+
+	for(var i=0; i<cards.length; i++) {
+
+	var cardElement = document.createElement ('div');
+
+	cardElement.className = 'card';
+
+	board.appendChild(cardElement);}
+
+
 }
 
-if (cardThree === cardFour) {
-	alert ('You found a match!');
-}
-else {
-	alert ('Sorry, try again.');
-}
+// if (cardTwo === cardFour) {
+// 	alert ('You found a match!');
+// }
+// else {
+// 	alert ('Sorry, try again.');
+// }
 
-if (cardOne === cardThree) {
-	alert ('You found a match!');}
+// if (cardOne === cardThree) {
+// 	alert ('You found a match!');
+// }
+// else {
+// 	alert ('Sorry, try again.');
+// }
 
-else  {
-	alert ('Sorry, try again.');
-}
+// if (cardOne === cardTwo) {
+// 	alert ('You found a match!');
+// }
+// else {
+// 	alert ('Sorry, try again.');
+// }
 
-if (cardTwo === cardThree) {
-	alert ('You found a match');
-}
-else {
-	alert ('Sorry, try again');
-}
+// if (cardThree === cardFour) {
+// 	alert ('You found a match!');
+// }
+// else {
+// 	alert ('Sorry, try again.');
+// }
+
+// if (cardOne === cardThree) {
+// 	alert ('You found a match!');}
+
+// else  {
+// 	alert ('Sorry, try again.');
+// }
+
+// if (cardTwo === cardThree) {
+// 	alert ('You found a match');
+// }
+// else {
+// 	alert ('Sorry, try again');
+// }
+
